c84e1363b8f2b02885d86532231117acf1815041
SalmanNouman <salmannouman510@gmail.com>
2026-03-15 21:35:45 -0400

    feat: add React Router, Tailwind CSS v4, and shared package integration to frontend

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,10 +1,22 @@
+/**
+ * @filename main.tsx
+ * @date 2026-03-15
+ * @author Salman Nouman Abulqasim
+ * @fileoverview Main entry point for the frontend application
+ * @version 1.0.0
+ */
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
+
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )

a2bb444c024f0e477931e8c5185742f1b09f325f
Karandeep Sandhu <ksandhu1054@conestogac.on.ca>
2026-03-28 17:54:32 -0400

    Add MediaScreen

---
+++ apps/frontend/src/features/media/MediaScreen.tsx
@@ -0,0 +1,9 @@
+import { AppHeader } from '../../components/AppHeader'
+
+export const MediaScreen = () => {
+  return (
+    <div className="relative min-h-screen overflow-hidden bg-[#f4f6f5] text-slate-900">
+      <AppHeader />
+    </div>
+  )
+}
